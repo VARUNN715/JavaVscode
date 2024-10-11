226a94e650ae8e6c46e2bdd114ca326ed2ebe117
VARUNN715 <varunn390@gmail.com>
2024-10-11 22:08:03 +0530

    adding comment

--- src/Sorting/Bubble.java
+++ src/Sorting/Bubble.java
@@ -6,9 +6,12 @@ public class Bubble {
         bubbleSort(arr);
     }
     public static void bubbleSort(int[] arr){
+
+        //time complexity = O(n^2)
         for(int i=0;i<arr.length-1;i++){
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[j] < arr[i]) {
+                    //swap
                     int temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
