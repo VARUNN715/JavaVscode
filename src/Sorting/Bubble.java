c2c93b9b0e51d0f220da50a5b8a9858813755b7f
VARUNN715 <varunn390@gmail.com>
2024-10-13 19:16:40 +0530

    bubble sort traced

--- src/Sorting/Bubble.java
+++ src/Sorting/Bubble.java
@@ -8,16 +8,16 @@ public class Bubble {
     public static void bubbleSort(int[] arr){
 
         //time complexity = O(n^2)
-        for(int i=0;i<arr.length-1;i++){
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    //swap
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }              
+       for(int i=0;i<arr.length-1;i++){
+        for(int j=i+1;j<arr.length;j++){
+            if (arr[j] < arr[i]) {
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
+       }
         for (int i = 0; i < arr.length; i++) { 
             System.out.print(arr[i]+" ");
         }
