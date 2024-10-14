66ad28a6be2af989891f25e593a1da2fda8b7656
VARUNN715 <varunn390@gmail.com>
2024-10-14 23:07:52 +0530

    slight changes

--- src/CollectionFrame/GenericMethodd.java
+++ src/CollectionFrame/GenericMethodd.java
@@ -4,6 +4,8 @@ public class GenericMethodd {
     public static void main(String[] args) {
         printData("Hello World!!");
         printData(21);
+        printData(21.2222222);
+        printData(3.14f);
     }
 
     static<E> void printData(E data){
